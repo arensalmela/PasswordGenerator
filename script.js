128d7db61bef504165444fc90948e6fcc901f8eb
Aren S <adsalmela@gmail.com>
2020-10-06 17:51:31 -0400

    Boiler plate Update. Pulled from develop folder.

--- script.js
+++ script.js
@@ -75,3 +75,17 @@ var specialcharacters = [
   "~",
 ];
 var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
