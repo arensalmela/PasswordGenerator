63f49af7f837ee183841fcc073bfcc5081abaa28
Aren S <adsalmela@gmail.com>
2020-10-10 15:14:14 -0400

    Added comments to JS and reset css file for proper practice.

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 const lowercaseletters = [
   "a",
   "b",
@@ -77,36 +79,33 @@ const specialcharacters = [
 
 const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
+// Prompting user input with conditionals. They select the length and which types of characters they want in their password.
+ 
 function passwordOptions() {
   let length = parseInt(
     prompt("Please select the number of characters for your password.")
   );
 
-  if (isNaN(length) === true) {
+  if (isNaN(length)) {
     alert("Please imput a number!");
     return;
   }
 
+  
   if (length < 8 || length > 128) {
     alert("Please select a number between 8 and 128.");
     return;
   }
   
-  let UserSelectedLwrCase = confirm(
-    "Would you like lowercase letters in your password?"
-  );
+  let UserSelectedLwrCase = confirm("Would you like lowercase letters in your password?");
 
-  let UserSelectedUprCase = confirm(
-    "Would you like uppercase letters in your password?"
-  );
+  let UserSelectedUprCase = confirm("Would you like uppercase letters in your password?");
 
-  let UserSelectedSpclChar = confirm(
-    "Would you like special characters in your password?"
-  );
-  let UserSelectedNmbr = confirm(
-    "Would you like special characters in your password?"
-  );
+  let UserSelectedSpclChar = confirm("Would you like special characters in your password?");
+
+  let UserSelectedNmbr = confirm("Would you like special characters in your password?");
   
+  // If statement to make sure user selected atleast one character field. 
 
   if (
     UserSelectedLwrCase === false &&
@@ -118,15 +117,19 @@ function passwordOptions() {
     return;
   }
 
+  // User input stored in SelectedRequirements.
+
   let SelectedRequirements = {
+    length,
     UserSelectedLwrCase,
     UserSelectedUprCase,
     UserSelectedSpclChar,
     UserSelectedNmbr,
-    length,
   };
   return SelectedRequirements;
 }
+
+// Randomizer to select random element from each array.
 
 function Random(arr) {
   let randomindex = Math.floor(Math.random() * arr.length);
@@ -134,40 +137,56 @@ function Random(arr) {
   return randomelement;
 }
 
+// This function will generate password with user input. Options variable stores user input from above. 
+
 function generatePassword() {
   let options = passwordOptions();
   let result = [];
   let possiblecharacters = [];
   let definitecharacters = [];
 
+  // Statements add to possible characters array based off of user input. 
+  // If user selected character type, one character will be pushed to definite character array.
+
   if (options.UserSelectedLwrCase) {
     possiblecharacters = possiblecharacters.concat(lowercaseletters);
     definitecharacters.push(Random(lowercaseletters));
+    console.log(possiblecharacters);
   }
 
   if (options.UserSelectedUprCase) {
     possiblecharacters = possiblecharacters.concat(uppercaseletters);
     definitecharacters.push(Random(uppercaseletters));
+    console.log(possiblecharacters);
   }
 
   if (options.UserSelectedSpclChar) {
     possiblecharacters = possiblecharacters.concat(specialcharacters);
     definitecharacters.push(Random(specialcharacters));
+    console.log(possiblecharacters);
   }
 
   if (options.UserSelectedNmbr) {
     possiblecharacters = possiblecharacters.concat(numbers);
     definitecharacters.push(Random(numbers));
+    console.log(possiblecharacters);
   }
+
+  // For loop used to select from possible characters array. Will run based off of user selected length.
 
   for (let i = 0; i < options.length; i++) {
     let possiblecharacter = Random(possiblecharacters);
     result.push(possiblecharacter);
   }
 
+  // Definite character being added to result.
+
   for (let i = 0; i < definitecharacters.length; i++) {
     result[i] = definitecharacters[i];
+    console.log(definitecharacters);
   }
+
+  // Result is now a string. 
 
   return result.join("");
 }
